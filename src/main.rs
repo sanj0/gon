f8413abe6a8b3808fa81f30bad9fc16205e6a515
Malte Dostal <malte.dostal@gmail.com>
2025-03-08 11:49:24 +0100

    add binary and spell features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::path::PathBuf;
+use std::io::Read;
+use std::fs::File;
+
+use clap::Parser;
+
+use gon::*;
+
+#[derive(Parser)]
+#[command(name = "gon", version, about = "CLI-utility for working with GON data", long_about = None)]
+struct Args {
+    /// What can I do for you?
+    verb: Verb,
+    /// The input file. Leave empty for stdin.
+    file: Option<PathBuf>,
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum Verb {
+    /// Minify the input
+    Min,
+    /// Format the input
+    Fmt,
+    /// Convert input to json
+    Json,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let Some(input) = get_input(args.file)? else {
+        return Ok(());
+    };
+    match args.verb {
+        Verb::Min => println!("{}", input.min_spell()),
+        Verb::Fmt => {
+            println!("{}", input.spell(SpellConfig::default())?);
+        }
+        Verb::Json => println!("{}", serde_json::to_string_pretty(&serde_json::Value::from(input)).map_err(|e| Box::new(e))?),
+    }
+    Ok(())
+}
+
+fn get_input(file: Option<PathBuf>) -> Result<Option<Value>, Box<dyn Error>> {
+    let src = if let Some(file) = file {
+        let file = File::open(file).map_err(|e| Box::new(e))?;
+        std::io::read_to_string(file).map_err(|e| Box::new(e))?
+    } else {
+        let mut input = Vec::new();
+        std::io::stdin().read_to_end(&mut input).map_err(|e| Box::new(e))?;
+        String::from_utf8(input).map_err(|e| Box::new(e))?
+    };
+    parse_str(&src).map_err(|e| e.into())
 }
--- src/value.rs
+++ src/value.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Value {
     None,
@@ -18,6 +20,14 @@ pub struct List {
     inner: Vec<Value>,
 }
 
+/// Configures how a `Value` should be [`spell`]ed
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct SpellConfig {
+    indent_amount: usize,
+    indent_char: char,
+    trailing_commas: bool,
+}
+
 impl Value {
     pub fn as_f64(&self) -> Option<f64> {
         let Self::Num(num) = self else {
@@ -31,5 +41,110 @@ impl Value {
             return None;
         };
         num.parse().ok()
+    }
+
+    /// Minimally spells this value
+    pub fn min_spell(&self) -> String {
+        match self {
+            Self::None => "None".into(),
+            Self::Str(s) => klex::Token::Str(s.into()).spelling(),
+            Self::Num(s) => s.into(),
+            Self::Bool(b) => if *b { "true".into() } else { "false".into() },
+            Self::Obj(m) => {
+                let mut spelling = String::from("{");
+                for (k, v) in m {
+                    spelling.push_str(k);
+                    spelling.push(':');
+                    spelling.push_str(&v.min_spell());
+                    spelling.push(',');
+                }
+                spelling.push('}');
+                spelling
+            }
+            Self::List(xs) => {
+                let mut spelling = String::from("[");
+                for v in xs {
+                    spelling.push_str(&v.min_spell());
+                    spelling.push(',');
+                }
+                spelling.push(']');
+                spelling
+            }
+        }
+    }
+
+    pub fn spell(&self, config: SpellConfig) -> Result<String, std::fmt::Error> {
+        let mut buf = String::new();
+        self.spell0(&mut buf, 0, &config)?;
+        Ok(buf)
+    }
+
+    fn spell0(&self, buf: &mut String, current_indent: usize, config: &SpellConfig) -> std::fmt::Result {
+        match self {
+            Self::None => write!(buf, "None")?,
+            Self::Str(s) => write!(buf, "{}", klex::Token::Str(s.clone()).spelling())?,
+            Self::Num(s) => write!(buf, "{s}")?,
+            Self::Bool(b) => write!(buf, "{b}")?,
+            Self::Obj(obj) => {
+                writeln!(buf, "{{")?;
+                let new_indent = current_indent + config.indent_amount;
+                for (i, (k, v)) in obj.iter().enumerate() {
+                    apply_indent(buf, new_indent, config)?;
+                    write!(buf, "{k}: ")?;
+                    v.spell0(buf, new_indent, config)?;
+                    if !config.trailing_commas && i == obj.len() - 1 {
+                        writeln!(buf, "")?;
+                    } else {
+                        writeln!(buf, ",")?;
+                    }
+                }
+                apply_indent(buf, current_indent, config)?;
+                write!(buf, "}}")?;
+            }
+            Self::List(xs) => 'match_arm: {
+                if xs.is_empty() {
+                    write!(buf, "[]")?;
+                    break 'match_arm;
+                }
+                let oneline = xs.len() <= 5 && xs.iter().find(|v| matches!(v, Self::List(_) | Self::Obj(_))).is_none();
+                if oneline {
+                    write!(buf, "[ ")?;
+                } else {
+                    writeln!(buf, "[")?;
+                }
+                for (i, x) in xs.iter().enumerate() {
+                    if oneline {
+                        x.spell0(buf, 0, config)?;
+                    } else {
+                        let new_indent = current_indent + config.indent_amount;
+                        apply_indent(buf, new_indent, config)?;
+                        x.spell0(buf, new_indent, config)?;
+                    }
+                    if config.trailing_commas || i < xs.len() - 1 {
+                        write!(buf, ",")?;
+                    }
+                    if !oneline {
+                        writeln!(buf, "")?;
+                    }
+                }
+                apply_indent(buf, current_indent, config)?;
+                write!(buf, "]")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+fn apply_indent(buf: &mut String, amount: usize, config: &SpellConfig) -> std::fmt::Result {
+    write!(buf, "{}", std::iter::repeat(config.indent_char).take(amount).collect::<String>())
+}
+
+impl Default for SpellConfig {
+    fn default() -> Self {
+        Self {
+            indent_amount: 4,
+            indent_char: ' ',
+            trailing_commas: false,
+        }
     }
 }
